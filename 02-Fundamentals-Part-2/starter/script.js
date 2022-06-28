df3a3ad9e227e241ac351fd30cd8eb28ab9ecbe9
michelangelo.pezzini@compasso.com.br <michelangelo.pezzini@compasso.com.br>
2022-06-28 08:45:54 -0300

    Initial commit

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,22 @@
+/* const bestFoods = ["Lasanha", "Hamburguer", "Pizza"];
+console.log(bestFoods[1]);
+console.log(bestFoods.length);
+
+console.log("Alteração feita no dell");
+ */
+
+/*Add elements */
+const friends = ["Mike", "Gabi", "Dudu"];
+friends.push("Denner");
+friends.unshift("Dany");
+console.log(friends);
+
+/*Remove elements */
+
+friends.pop();
+friends.shift()
+console.log(friends );
+
+/*Show the position of the array */
+
+console.log(friends.indexOf("Gabi"))
